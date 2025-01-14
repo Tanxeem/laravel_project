e1687cc969df0bc93867a591480f23133dc5df89
tanxeem <you@example.com>
2025-01-14 23:21:54 +0300

    index html page

--- resources/views/pages/kids.blade.php
+++
@@ -1,9 +0,0 @@
-@vite('resources/css/app.css')
-@extends('layouts.main')
-
-
-@section('main-container')
-
-<h1>Kids Page Here</h1>
-
-    @endsection
--- resources/views/pages/women.blade.php
+++
@@ -1,9 +0,0 @@
-@vite('resources/css/app.css')
-@extends('layouts.main')
-
-
-@section('main-container')
-
-<h1>Woemn Page Here</h1>
-
-    @endsection
